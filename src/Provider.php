c5215fc9ab09daf7ae5126ce06b02eb30de3488d
Andy Wendt <andy@awendt.com>
2015-02-21 15:51:46 -0700

    Initial Commit

---
+++ src/Provider.php
@@ -0,0 +1,73 @@
+<?php
+namespace SocialiteProviders\WordPress;
+
+use Laravel\Socialite\Two\AbstractProvider;
+use Laravel\Socialite\Two\ProviderInterface;
+use Laravel\Socialite\Two\User;
+
+class Provider extends AbstractProvider implements ProviderInterface
+{
+    /**
+     * The scopes being requested.
+     *
+     * @var array
+     */
+    protected $scopes = [];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase('https://public-api.wordpress.com/oauth2/authorize', $state);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return 'https://public-api.wordpress.com/oauth2/token';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get(
+            'https://public-api.wordpress.com/rest/v1/me',
+            [
+                'headers' => [
+                    'Authorization' => 'Bearer '.$token,
+                ],
+            ]
+        );
+
+        return json_decode($response->getBody(), true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map(
+            [
+                'id' => $user['ID'],
+                'nickname' => $user['username'],
+                'name' => $user['display_name'],
+                'email' => isset($user['email']) ? $user['email'] : null,
+                'avatar' => $user['avatar_URL'],
+            ]
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenFields($code)
+    {
+        return array_merge(parent::getTokenFields($code), ['grant_type' => 'authorization_code']);
+    }
+}
